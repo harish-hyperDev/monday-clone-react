1de3e4f498a5160dd6637b02fdb0750dbcc69d3c
harish-hyperDev <coolprogrammer.777@gmail.com>
2023-01-13 19:01:17 +0530

    modifications to TicketCard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
     {
       category: 'Q1 2022',
       color: 'red',
-      title: 'MRF Safety 01KA',
+      title: 'Messenger App Clone - React',
       owner: 'Ice Fruit',
       avatar: 'https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=740&t=st=1673524495~exp=1673525095~hmac=c1a9c5b223c3c7597f433bf62fa0015b828cf82d9f6f448718baef4f80ae15aa',
       status: 'done',
@@ -27,6 +27,18 @@ const Dashboard = () => {
       progress: 80,
       description: "Ice Fruit Ticket",
       timestamp: '2022-04-15T12:14:11'
+    },
+    {
+      category: 'Q2 2022',
+      color: 'green',
+      title: 'BMW Safety 11FC',
+      owner: 'Ice Fruit',
+      avatar: 'https://img.freepik.com/free-psd/3d-rendering-travel-tourist_23-2149667951.jpg?w=740&t=st=1673524724~exp=1673525324~hmac=ac9807c1b67f77dfe2f56537cfe986105571abec72fd33f3f340c4f3a3a60dc5',
+      status: 'in-progress',
+      priority: 2,
+      progress: 80,
+      description: "Ice Fruit Ticket",
+      timestamp: '2022-04-15T12:14:11'
     }
   ]
 
@@ -38,7 +50,7 @@ const Dashboard = () => {
     
 
     <div className='dashboard p-5 w-full'>
-      <h1>My Projects</h1>
+      {/* <h1>My Projects</h1> */}
       <div className='ticket-container h-[80vh] overflow-scroll'>
         {/* <TicketCard /> */}
         {tickets && uniqueCategories?.map((uniqueCategory, categoryIndex) => {
